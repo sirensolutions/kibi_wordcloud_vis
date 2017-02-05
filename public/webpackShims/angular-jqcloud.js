aeee0deb74231e4d055a33b46666e5f6dcb6fe25
szydan <danielczyk.szymon@gmail.com>
2017-02-05 11:55:05 +0000

    Fixed angular-jqcloud shim

--- public/webpackShims/angular-jqcloud.js
+++ public/webpackShims/angular-jqcloud.js
@@ -1,5 +1,9 @@
-require('../../node_modules/jqcloud2/src/jqcloud');
-require('../../node_modules/jqcloud2/src/jqcloud.css');
+require('angular');
+require('jquery');
+// NOTE: without the "script!" webpack is adding extra jquery to the bundle
+// because it is required when loading jqcloud2 in node_modules/jqcloud2/dist/jqcloud.js
+require('script!../../node_modules/jqcloud2/dist/jqcloud');
+require('../../node_modules/jqcloud2/dist/jqcloud.css');
 require('../../node_modules/angular-jqcloud/angular-jqcloud');
 
 require('ui/modules').get('kibana', ['angular-jqcloud']);
